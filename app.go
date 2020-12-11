900dfa90e529d2f1ac3a5e33ef3cc785932058b2
Mengen Gao <gaomengen@gmail.com>
2020-12-11 01:17:29 -0500

    dao structure done

--- app.go
+++ app.go
@@ -7,11 +7,9 @@ import (
 	"encoding/json"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/gaomengen/cloudhired-backend/dao"
-	"github.com/gaomengen/cloudhired-backend/models"
+	"cloudhired.com/api/dao"
+	// "cloudhired.com/api/models"
 )
-
-var dao = UsersDao{}
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
     fmt.Fprint(w, "Welcome!\n")
@@ -21,20 +19,36 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
-func AllUsers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	users, err := dao.FindAllUsers()
-	if err != nil {
-		return
-	}
-	respondWithJson(w, http.StatusOK, users)
+// func AllUsers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+// 	users, err := dao.FindAllUsers()
+// 	if err != nil {
+// 		return
+// 	}
+// 	respondWithJson(w, http.StatusOK, users)
 
+// }
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func init() {
+
+	// dao.Server = "localhost"
+	// dao.Database = "users"
+	// dao.Connect()
 }
 
 func main() {
     router := httprouter.New()
     router.GET("/", Index)
     router.GET("/hello/:name", Hello)
-	router.GET("/users", AllUsers)
+	// router.GET("/users", AllUsers)
+
+	dao.TestDb()
 
     log.Fatal(http.ListenAndServe(":8081", router))
 }
--- dao/users_dao.go
+++ dao/users_dao.go
@@ -1,15 +1,56 @@
 package dao
 
 import (
-	"../models"
-)
+	"context"
+	"fmt"
+	"log"
+	// "cloudhired.com/api/models"
 
-type UsersDAO struct {
-	Server 		string
-	Database	string 
-}
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 const (
+	CONNECTIONSTRING = "mongodb+srv://ch-user:FhDne1WoX3qI2wIm@cloudhired.c58f7.gcp.mongodb.net/cloudhired?retryWrites=true&w=majority"
+	DBNAME = ""
 	COLLECTION =  "users"
 )
+
+var db *mongo.Database
+
+// Connect establish a connection to database
+func init() {
+	clientOptions := options.Client().ApplyURI(CONNECTIONSTRING)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Collection types can be used to access the database
+	db = client.Database("cloudhired")
+}
+
+func TestDb() {
+	collection := db.Collection("users")
+	findOptions := options.Find()
+	findOptions.SetLimit(10)
+	cur, err := collection.Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []bson.M
+	if err = cur.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+	}
+	for _, result := range results {
+		fmt.Println(result)
+	}
+
+	for cur.Next(context.TODO()) {
+		var elem bson.M
+		fmt.Println(elem)
+	}
+}
 
