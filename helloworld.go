ad3c7839dfac3ceb2bf341d132d3876f16c749ae
gaomengen <gaomengen@gmail.com>
2020-12-14 10:19:22 -0500

    correct one

--- helloworld.go
+++ helloworld.go
@@ -25,7 +25,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
-	"cloudhired.com/api/dao1"
 )
 
 func helloworld() {
