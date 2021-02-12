2007f6954a38afb6cbd543b6ff84f75180f44491
gaomengen <gaomengen@gmail.com>
2021-02-12 13:04:18 -0500

    testing gcp secret manager

--- config/config.go
+++ config/config.go
@@ -1,5 +1,34 @@
 package config
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+)
+
 const (
 	ConnectionString = "mongodb+srv://ch-user:FhDne1WoX3qI2wIm@cloudhired.c58f7.gcp.mongodb.net/cloudhired?retryWrites=true&w=majority"
 )
+
+func init() {
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	fmt.Println("here")
+	if err != nil {
+		log.Fatal("client: ", err)
+	}
+
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: "projects/782780515351/secrets/my-secret",
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		log.Fatal("the Error: ", err)
+	}
+
+	log.Fatal("result: ", string(result.Payload.Data))
+}
--- go.mod
+++ go.mod
@@ -3,16 +3,14 @@ module cloudhired.com/api
 go 1.15
 
 require (
+	cloud.google.com/go v0.76.0
 	github.com/aws/aws-sdk-go v1.36.2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f // indirect
 	go.mongodb.org/mongo-driver v1.4.4
-	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
-	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
-	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20210211221406-4ccc9a5e4183
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
