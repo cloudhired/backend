0a92f035be703501ff6e475699e86e71ee4d6d24
gaomengen <gaomengen@gmail.com>
2021-02-12 14:07:48 -0500

    changed service account for cloud run. try again

--- config/config.go
+++ config/config.go
@@ -1,34 +1,25 @@
 package config
 
-import (
-	"context"
-	"fmt"
-	"log"
-
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
-)
-
 const (
 	ConnectionString = "mongodb+srv://ch-user:FhDne1WoX3qI2wIm@cloudhired.c58f7.gcp.mongodb.net/cloudhired?retryWrites=true&w=majority"
 )
 
-func init() {
-	ctx := context.Background()
-	client, err := secretmanager.NewClient(ctx)
-	fmt.Println("here")
-	if err != nil {
-		log.Fatal("client: ", err)
-	}
-
-	req := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: "projects/782780515351/secrets/my-secret/versions/1",
-	}
-
-	result, err := client.AccessSecretVersion(ctx, req)
-	if err != nil {
-		log.Fatal("the Error: ", err)
-	}
-
-	log.Fatal("secret result: ", string(result.Payload.Data))
-}
+//func init() {
+//	ctx := context.Background()
+//	client, err := secretmanager.NewClient(ctx)
+//	fmt.Println("here")
+//	if err != nil {
+//		log.Fatal("client: ", err)
+//	}
+//
+//	req := &secretmanagerpb.AccessSecretVersionRequest{
+//		Name: "projects/782780515351/secrets/my-secret/versions/1",
+//	}
+//
+//	result, err := client.AccessSecretVersion(ctx, req)
+//	if err != nil {
+//		log.Fatal("the Error: ", err)
+//	}
+//
+//	log.Fatal("secret result: ", string(result.Payload.Data))
+//}
