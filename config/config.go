fef903ed45f8bd33135ff2fb61e9572560706459
gaomengen <gaomengen@gmail.com>
2021-02-12 20:36:40 -0500

    ready to deploy - test clogger

--- config/config.go
+++ config/config.go
@@ -4,9 +4,11 @@ import (
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloudhired.com/api/clogger"
 	"context"
-	"fmt"
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
-	"log"
+)
+
+const (
+	CONNSTRING = "projects/782780515351/secrets/my-secret/versions/latest"
 )
 
 var ConnectionString string
@@ -28,15 +30,11 @@ func init() {
 	clogger.Debug("debugging...")
 
 	req := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: "projects/782780515351/secrets/my-secret/versions/2",
+		Name: CONNSTRING,
 	}
-
 	result, err := client.AccessSecretVersion(ctx, req)
 	if err != nil {
-		log.Fatal("the Error: ", err)
+		clogger.Error(err)
 	}
-
 	ConnectionString = string(result.Payload.Data)
-
-	fmt.Println("secret result: ", string(result.Payload.Data))
 }
